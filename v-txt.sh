72bfdd1303c95890fc6211f499ec9f61a9a8ff1a
iXploitID <93501879+iXploitID@users.noreply.github.com>
2021-11-21 16:45:57 +0700

    Add files via upload

---
+++ v-txt.sh
@@ -0,0 +1,127 @@
+clear
+
+echo -e "\E[?25l\E[44m\E[2J"
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                             
+yellow='\033[33;1m'
+figlet VIRTEX
+echo
+echo -e $cyan"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e "1"
+echo -e $cyan"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e "2"
+echo -e $cyan"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e "3"
+echo -e $cyan"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e "4"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $green "5"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $purple "6"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $cyan "7"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $red "8"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $white "9"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $green "10"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $purple "11"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $cyan "12"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e  $white "13.Pilih SEMUA + BONUS"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e "Ketik 0 untuk keluar dari tools"
+echo -e  "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e 'NOTE YANG DI MANGSUT SDCARD DI TERMUX ITU PENYIMPANAN INTERNAL'
+echo
+echo
+
+
+
+
+
+
+echo -e $purple "╭─{màşùķàñ płïhàñ mù]"
+read -p " ╰─> " act;
+
+
+if [ $act = 1 ] || [ $act = 1 ]
+then
+cp -f 1.txt /sdcard
+echo -e "Tersimpan di sdcard 1.txt"
+fi
+if [ $act = 2 ] || [ $act = 2 ]
+then
+cp -f 2.txt /sdcard
+echo -e "Tersimpan di sdcard 2.txt"
+fi
+if [ $act = 3 ] || [ $act = 3 ]             
+then
+cp -f 3.txt /sdcard
+echo -e "Tersimpan di sdcard 3.txt"
+fi
+if [ $act = 4 ] || [ $act = 4 ]
+then
+cp -f 4.txt /sdcard
+echo -e "Tersimpan di sdcard 4.txt"
+fi
+if [ $act =  5 ] || [ $act = 5 ]
+then
+cp -f 5.txt /sdcard
+echo -e "Tersimpan di sdcard 5.txt"
+fi
+if [ $act = 6 ] || [ $act = 6 ]
+then
+cp -f 6.txt /sdcard
+echo -e "Tersimpan di sdcard 6.txt"
+fi
+if [ $act = 7 ] || [ $act = 7 ]
+then
+cp -f 7.txt /sdcard
+echo -e "Tersimpan di sdcard 7.txt"
+fi
+if [ $act = 8 ] || [ $act = 8 ]
+then
+cp -f 8.txt /sdcard
+echo -e "Tersimpan di sdcard 8.txt"
+fi
+if [ $act = 9 ] || [ $act = 9 ]
+then
+cp -f 9.txt /sdcard
+echo -e "Tersimpan di sdcard 9.txt"
+fi
+if [ $act = 10 ] || [ $act = 10 ]
+then
+cp -f 10.txt /sdcard
+echo -e "Tersimpan di sdcard 10.txt"
+fi
+if [ $act = 11 ] || [ $act = 11 ]
+then
+cp -f 11.txt /sdcard
+echo -e "Tersimpan di sdcard 11.txt"
+fi
+if [ $act = 12 ] || [ $act = 12 ]
+then
+cp -f 12.txt /sdcard
+echo -e "Tersimpan di sdcard 12.txt"
+fi
+if [ $act = 13 ] || [ $act = 13 ]
+then
+cp -f virtex.zip /sdcard
+echo -e "Tersimpan di sdcard virtex.zip"
+fi
+if [ $act = 0 ] || [ $act = 0 ]
+then
+clear
+exit
+login
+clear
+login
+fi
